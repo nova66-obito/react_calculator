43539f3b97e9d7a34f022c6331b7144eb0d97d37
nova66-obito <sambathvj0506@gmail>
2025-06-07 21:40:11 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css';
 import Home from './pages/index';
-import Dub from './pages/dub';
 function App() {
  
   return (
